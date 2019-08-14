8b5c9d2ca2272147abc4cbc32eddea1b279f0284
Ranu <O@O.net>
2019-08-14 12:09:03 +0100

    Pokedex solution completed

---
+++ bootcamp/05-pokedex-project/pokedex-project-exercise/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Pokegame from './Pokegame';
+
+function App() {
+  return (
+    <div className="App">
+      <Pokegame />
+    </div>
+  );
+}
+
+export default App;
